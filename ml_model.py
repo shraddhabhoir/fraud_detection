e2495b1c6dd667c3e3502ca590600f0f9fd8ce2c
shraddhabhoir <er.shraddhabhoir@gmail.com>
2022-03-26 01:00:24 +0530

    added 3 new ml functions

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding machine learning related code")
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Model3")
